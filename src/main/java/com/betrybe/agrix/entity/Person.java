8b782867b12374af8b6102f73ec2808b2d498676
Bruno Marcadella <bruno.marcadella@hotmail.com>
2024-09-06 14:25:58 -0300

    Requisito 3 Finalizado

--- src/main/java/com/betrybe/agrix/entity/Person.java
+++ src/main/java/com/betrybe/agrix/entity/Person.java
@@ -7,13 +7,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * Class representing a person.
  */
 @Entity
-public class Person {
+public class Person implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,37 +30,97 @@ public class Person {
 
   private Role role;
 
+  /**
+   * Instantiates a new Person.
+   */
   public Person() {
   }
 
+  /**
+   * Instantiates a new Person.
+   *
+   * @param id       the id
+   * @param username the username
+   * @param password the password
+   * @param role     the role
+   */
+  public Person(Long id, String username, String password, Role role) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.role = role;
+  }
+
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
   public Long getId() {
     return id;
   }
 
+  /**
+   * Sets id.
+   *
+   * @param id the id
+   */
   public void setId(Long id) {
     this.id = id;
   }
 
+  /**
+   * Gets username.
+   *
+   * @return the username
+   */
+  @Override
   public String getUsername() {
-    return username;
+    return this.username;
   }
 
+  /**
+   * Sets username.
+   *
+   * @param username the username
+   */
   public void setUsername(String username) {
     this.username = username;
   }
 
+  /**
+   * Gets password.
+   *
+   * @return the password
+   */
+  @Override
   public String getPassword() {
-    return password;
+    return this.password;
   }
 
+  /**
+   * Sets password.
+   *
+   * @param password the password
+   */
   public void setPassword(String password) {
     this.password = password;
   }
 
+  /**
+   * Gets role.
+   *
+   * @return the role
+   */
   public Role getRole() {
     return role;
   }
 
+  /**
+   * Sets role.
+   *
+   * @param role the role
+   */
   public void setRole(Role role) {
     this.role = role;
   }
@@ -73,6 +137,31 @@ public class Person {
     return Objects.equals(id, person.id) && Objects.equals(username,
         person.username) && Objects.equals(password, person.password)
         && Objects.equals(role, person.role);
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 }
 
