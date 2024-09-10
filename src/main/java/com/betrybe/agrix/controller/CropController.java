fe08abdfa2d780216c42039bfbb842d754e74200
Bruno Marcadella <bruno.marcadella@hotmail.com>
2024-09-09 23:55:32 -0300

    Requisito 4 Corrigido e 5 Finalizado

--- src/main/java/com/betrybe/agrix/controller/CropController.java
+++ src/main/java/com/betrybe/agrix/controller/CropController.java
@@ -11,6 +11,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,6 +61,7 @@ public class CropController {
    * @return the all crops
    */
   @GetMapping
+  @PreAuthorize("hasAuthority('MANAGER') or hasAuthority('ADMIN')")
   public List<CropDto> getAllCrops() {
     List<Crop> allCrops = cropService.findAll();
     return allCrops.stream()
--- src/main/java/com/betrybe/agrix/controller/FarmController.java
+++ src/main/java/com/betrybe/agrix/controller/FarmController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -77,6 +78,7 @@ public class FarmController {
    * @return the all farms
    */
   @GetMapping
+  @PreAuthorize("hasAuthority('USER') or hasAuthority('MANAGER') or hasAuthority('ADMIN')")
   public List<FarmDto> getAllFarms() {
     List<Farm> allFarms = farmService.findAll();
     return allFarms.stream()
