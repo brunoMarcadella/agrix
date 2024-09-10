9668dc5eec2c9cc569ff2cf70359d8c521a5f4b9
Bruno Marcadella <bruno.marcadella@hotmail.com>
2024-09-09 23:56:53 -0300

    Requisito 6 Finalizado

--- src/main/java/com/betrybe/agrix/controller/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/controller/FertilizerController.java
@@ -8,6 +8,7 @@ import com.betrybe.agrix.service.exception.FertilizerNotFoundException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -55,6 +56,7 @@ public class FertilizerController {
    * @return the all fertilizers
    */
   @GetMapping
+  @PreAuthorize("hasAuthority('ADMIN')")
   public List<FertilizerDto> getAllFertilizers() {
     List<Fertilizer> allFertilizers = fertilizerService.findAll();
 
